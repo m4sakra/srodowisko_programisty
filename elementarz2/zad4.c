be1f6de97e9820a7991147fd49a313078964222e
m4sakra <masakra96l2@gmail.com>
2017-01-14 00:15:35 +0100

    Add files via upload

---
+++ elementarz2/zad4.c
@@ -0,0 +1,18 @@
+/* Bylo w ele1 - dynia to dynia... */
+#include <stdio.h>
+int main ()
+{
+  int nb, nt, nl;
+  int c;
+  nt = 0;
+  nl = 0;
+  nb = 0;
+  while ((c=getchar()) != EOF)
+  if (c == ' ')
+  ++nb;
+  else if (c == '\t')
+  ++nt;
+  else if (c == '\n')
+  ++nl;
+  printf("Liczba znakow odstępu = %d\nLiczba znakow tabulacji = %d\nLiczba znakow nowego wiersza = %d\n", nb, nt, nl);
+}
